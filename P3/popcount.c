5289f9d85089fa6b150eae8d9451d5a697010fcf
odeclasonarres <odeclasonarres@gmail.com>
2016-11-27 22:57:35 +0100

    P3 acabada

--- P3/popcount.c
+++ P3/popcount.c
@@ -107,16 +107,16 @@ int popcount5(unsigned* array, int len){
 			"movdqu     %[m], %%xmm6 \n\t"
 			"psrlw       $4 , %%xmm1 \n\t"
 			"pand     %%xmm6, %%xmm0 \n\t"
-			"pand     %%xmm6, %%xmm1 \n\t" //; xmm1 â€“ nibbles superiores
-			"movdqu     %[l], %%xmm2 \n\t" //; ...como pshufb sobrescribe LUT
-			"movdqa   %%xmm2, %%xmm3 \n\t" //; ...queremos 2 copias
-			"pshufb   %%xmm0, %%xmm2 \n\t" //; xmm2 = vector popcount inferiores
-			"pshufb   %%xmm1, %%xmm3 \n\t" //; xmm3 = vector popcount superiores
-			"paddb    %%xmm2, %%xmm3 \n\t" //; xmm3 -vector popcount bytes
-			"pxor     %%xmm0, %%xmm0 \n\t" //; xmm0 =0,0,0,0
-			"psadbw   %%xmm0, %%xmm3 \n\t" //; xmm3 =[pcnt bytes0..7|pcnt bytes8..15]
-			"movhlps  %%xmm3, %%xmm0 \n\t" //; xmm0 = [ 0 |pcnt bytes0..7 ]
-			"paddd    %%xmm3, %%xmm0 \n\t" //; xmm0 = [ no usado |pcnt bytes0..15]
+			"pand     %%xmm6, %%xmm1 \n\t" 
+			"movdqu     %[l], %%xmm2 \n\t" 
+			"movdqa   %%xmm2, %%xmm3 \n\t" 
+			"pshufb   %%xmm0, %%xmm2 \n\t" 
+			"pshufb   %%xmm1, %%xmm3 \n\t" 
+			"paddb    %%xmm2, %%xmm3 \n\t" 
+			"pxor     %%xmm0, %%xmm0 \n\t" 
+			"psadbw   %%xmm0, %%xmm3 \n\t" 
+			"movhlps  %%xmm3, %%xmm0 \n\t" 
+			"paddd    %%xmm3, %%xmm0 \n\t" 
 			"movd     %%xmm0, %[val] \n\t"
 			:[val]"=r" (val)
 			:[x]"m"(array[i]),[m]"m" (SSE_mask[0]),[l]"m"(SSE_LUTb[0])
@@ -155,7 +155,6 @@ int main(){
     crono(popcount1, "popcount1 (en lenguaje C  -for)");
     crono(popcount2, "popcount2 (en lenguaje C  -while)");
     crono(popcount3, "popcount3 (bloque asm entero)");
-    //printf("aquí estoy");
     crono(popcount4, "popcount4 (en C, mascara)");
     crono(popcount5, "popcount5 (SSSE3)");
     	
